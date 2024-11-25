88fb3a24726e9c087a0deabd47db9ff661a9878c
medaminelechhab <150515112+medaminelechhab@users.noreply.github.com>
2024-11-25 11:34:56 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("aboudiiiiiiiiiiii !");
+        System.out.println("aboudiiiiiiiiiiii     !");
     }
 }
